2f9baefca13fc271d37c28f2eb77cac31de06ca6
suph4rd <suph4rd575@gmail.com>
2022-02-07 00:19:26 +0300

    fix: change message handlers

--- start.py
+++ start.py
@@ -16,13 +16,13 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
-    await message.reply("Welcome to the club! It's NB4444 bot, made by Andrey Bernyak")
+    await message.reply("Welcome to the club! It's NB4444 bot, made by Andrey Bernyak!")
 
 
 @dp.message_handler()
 async def main_handler(message: types.Message):
     await sync_to_async(Note.objects.create)(text=message.text)
-    await message.answer(message.text)
+    await message.answer("Your message saved successful!")
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
